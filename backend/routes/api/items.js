d59d8e2211481ddfe76cce61f3f47043263bd55e
yinho999 <yinho999@gmail.com>
2022-11-20 13:56:20 +0000

    finished

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -40,6 +40,11 @@ router.get("/", auth.optional, function(req, res, next) {
   var query = {};
   var limit = 100;
   var offset = 0;
+  var title = ""
+
+  if (typeof req.query.title !== "undefined") {
+    query.title = req.query.title
+  }
 
   if (typeof req.query.limit !== "undefined") {
     limit = req.query.limit;
